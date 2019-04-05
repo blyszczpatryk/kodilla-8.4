9b568ff4ca133c5836f637f11f3595ce0d4e1c84
Patryk <blyszczpatryk@gmail.com>
2019-04-05 17:51:05 +0200

    Delete file

--- icon-change.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var icons = document.getElementsById('rock-icon');
-
-var buttons = document.getElementsById('rock');
-
-buttons.addEventListener('click', function() {
-	icons.classList.add('icon-change');
-});
